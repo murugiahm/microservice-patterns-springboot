b0fcae1f718d1a65aadee56f5a3c73639d0bbbaf
Murugiah Murugathasan <murugiah.murugathasan@gmail.com>
2025-02-27 19:19:07 +0530

    Initial commit for Spring Boot microservice patterns

---
+++ circuit-breaker/src/test/java/com/murum/microservice/springboot/patterns/circuitbreaker/CircuitBreakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murum.microservice.springboot.patterns.circuitbreaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitBreakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
